28b5945c37a4bb49c6f74004d75d1b83b95b6693
p.m.student <cedric.mitchell92@jenksms.com>
2021-01-26 13:37:39 -0600

    Finish with the basics

---
+++ TacTicToe/TacTicToe/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  TacTicToe
+//
+//  Created by P.M. Student on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView {
+            Home()
+                .navigationTitle("Tac Tic Toe")
+            
+        }
+       
+    }
+}
+
+struct Home : View {
+    var body: some View {
+        VStack {
+            
+            LazyVGrid(columns: Array(repeating:GridItem(.flexible(), spacing:15), count:3), spacing: 15) {
+                
+                ForEach(0..<9, id: \.self)
+                { index in
+                    Color.black
+                        .frame(width: getWidth(),height: getWidth())
+                        .cornerRadius(20)
+                }
+                
+            }
+            
+            .padding()
+        }
+        
+    }
+    
+    // Used to calculate width.
+    
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    
+}
+}
